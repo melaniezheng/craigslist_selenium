c39753c701865322e56a21412a0ae959581eb7a0
Melanie Zheng <meilingjung@gmail.com>
2020-01-30 21:15:25 -0500

    added 4 cities

--- craigslist_selenium.py
+++ craigslist_selenium.py
@@ -149,10 +149,10 @@ class CraiglistScraper(object):
         self.driver.close()
     
     @staticmethod
-    def write_to_tsv(all_posts):
+    def write_to_tsv(all_posts, location):
         now = datetime.now()
         file_date = now.strftime("%Y-%m-%d")
-        filepath = "./data/" + file_date + "_" + boro + "_temp.tsv"
+        filepath = "./data/" + location + "_"+ file_date + "_" + boro + ".tsv"
         fieldnames = ['url','title','date','boro','neighborhood','price','size','bedrooms','cats_allowed','dogs_allowed', 'no_fee']
         try:
             with open(filepath, "w") as output_file:
@@ -163,48 +163,55 @@ class CraiglistScraper(object):
             print("Problem writing to tsv file")
 
 
-location = "newyork"
-boro_list = ["mnh","brk","que","brx","stn"] #brk for BK, que for Queens, brx for Bronx, stn for Staten Island
+cities = {}
+cities['newyork'] = ["mnh","brk","que","brx","stn"]
+cities['losangeles'] = ["wst","sfv","lac","sgv","lgb","ant"] 
+cities['chicago'] = ["chc","nch", "wcl", "sox", "nwi", "nwc"]
+cities['sfbay'] = ["sfc", "sby", "eby", "pen", "nby", "scz"]
 min_price = "500"
-max_price = "10000"
+max_price = "5000"
 min_bedrooms = "0"
 max_bedrooms = "6"
 
-for boro in boro_list:
-    url = f"https://{location}.craigslist.org/search/{boro}/apa?&min_price={min_price}&max_price={max_price}&min_bedrooms={min_bedrooms}&max_bedrooms={max_bedrooms}"
-    scraper = CraiglistScraper(boro, url)
-    
-    # print(url_lst) for debugging purpose
-    dog_allowed_url = url + "&pets_dog=1"
-    dog_allowed_url_lst = scraper.generate_url_lst(dog_allowed_url)
-    dogs_ok_listings = set()
-    for dogs_allowed_url in dog_allowed_url_lst:
-        scraper.load_url(dogs_allowed_url)
-        dogs_ok_listings.update(scraper.filter_ok_listings(dogs_allowed_url))
+for city in cities:
+    location = city
+    boro_list = cities[city]
+    for boro in boro_list:
+        url = f"https://{location}.craigslist.org/search/{boro}/apa?&min_price={min_price}&max_price={max_price}&min_bedrooms={min_bedrooms}&max_bedrooms={max_bedrooms}"
 
-    cat_allowed_url = url + "&pets_cat=1"
-    cat_allowed_url_lst = scraper.generate_url_lst(cat_allowed_url)
-    cats_ok_listings = set()
-    for cat_allowed_url in cat_allowed_url_lst:
-        scraper.load_url(cat_allowed_url)
-        cats_ok_listings.update(scraper.filter_ok_listings(cat_allowed_url))
+        scraper = CraiglistScraper(boro, url)
+        
+        # print(url_lst) for debugging purpose
+        dog_allowed_url = url + "&pets_dog=1"
+        dog_allowed_url_lst = scraper.generate_url_lst(dog_allowed_url)
+        dogs_ok_listings = set()
+        for dogs_allowed_url in dog_allowed_url_lst:
+            scraper.load_url(dogs_allowed_url)
+            dogs_ok_listings.update(scraper.filter_ok_listings(dogs_allowed_url))
 
-    no_fee_url = url + "&broker_fee=1"
-    no_fee_url_lst = scraper.generate_url_lst(no_fee_url)
-    no_fee_listings = set()
-    for no_fee_url in no_fee_url_lst:
-        scraper.load_url(no_fee_url)
-        no_fee_listings.update(scraper.filter_ok_listings(no_fee_url))
-    
-    url_lst = scraper.generate_url_lst(url)
-    all_posts = []
-    for url in url_lst:
-        scraper.load_url(url)
-        all_posts.append(scraper.extract_posts(url, dogs_ok_listings, cats_ok_listings, no_fee_listings))
-    all_posts = list(it.chain(*all_posts))
-    print("length of all posts: ") # debugging
-    print(len(all_posts)) # debugging
-    CraiglistScraper.write_to_tsv(all_posts)
+        cat_allowed_url = url + "&pets_cat=1"
+        cat_allowed_url_lst = scraper.generate_url_lst(cat_allowed_url)
+        cats_ok_listings = set()
+        for cat_allowed_url in cat_allowed_url_lst:
+            scraper.load_url(cat_allowed_url)
+            cats_ok_listings.update(scraper.filter_ok_listings(cat_allowed_url))
+
+        no_fee_url = url + "&broker_fee=1"
+        no_fee_url_lst = scraper.generate_url_lst(no_fee_url)
+        no_fee_listings = set()
+        for no_fee_url in no_fee_url_lst:
+            scraper.load_url(no_fee_url)
+            no_fee_listings.update(scraper.filter_ok_listings(no_fee_url))
+        
+        url_lst = scraper.generate_url_lst(url)
+        all_posts = []
+        for url in url_lst:
+            scraper.load_url(url)
+            all_posts.append(scraper.extract_posts(url, dogs_ok_listings, cats_ok_listings, no_fee_listings))
+        all_posts = list(it.chain(*all_posts))
+        print("length of all posts: ") # debugging
+        print(len(all_posts)) # debugging
+        CraiglistScraper.write_to_tsv(all_posts,location)
 
 scraper.quit()
 
